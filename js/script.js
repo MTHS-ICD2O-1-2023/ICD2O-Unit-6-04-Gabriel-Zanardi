415f56914e6a8bb92e192f0786a7e9e7c1826f4a
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-05-24 18:01:54 +0000

    Init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+
+  //input
+  let firstNumber = parseInt(document.getElementById("first-number").value)
+  let secondNumber = parseInt(document.getElementById("second-number").value)
+
+  //process
+  let counter = 0
+  let result = 0
+
+  while (counter < secondNumber) {
+    result += firstNumber
+    counter += 1
+  }
+
+  let finalResult = result
+
+  // output 
+  document.getElementById("final-answer").innerHTML = "Your result is " + finalResult
+}
